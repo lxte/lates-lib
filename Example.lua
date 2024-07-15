35f572b05db35c26dd73d45a11472a0a21e5a94f
late <162959866+lxte@users.noreply.github.com>
2024-07-15 10:21:52 +0300

    better sizing

--- Example.lua
+++ Example.lua
@@ -1,30 +1,53 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/lxte/lates-lib/main/Main.lua"))()
+local Library = require(script.Parent)
 local Window = Library:CreateWindow({
 	Title = "Arc",
-	Size = UDim2.fromScale(0.481, 0.526),
-	Transparency = 0.15,
+	Size = UDim2.fromOffset(500, 300),
+	Transparency = 0.2,
 	Blurring = true,
-	Theme = "Dark",
+	Theme = "Light",
 })
 
-local CustomTheme = {
-	--// Frames:
-	Primary = Color3.fromRGB(30, 30, 30),
-	Secondary = Color3.fromRGB(35, 35, 35),
-	Component = Color3.fromRGB(40, 40, 40),
-	Interactables = Color3.fromRGB(45, 45, 45),
+local Themes = {
+	Light = {
+		--// Frames:
+		Primary = Color3.fromRGB(232, 232, 232),
+		Secondary = Color3.fromRGB(255, 255, 255),
+		Component = Color3.fromRGB(245, 245, 245),
+		Interactables = Color3.fromRGB(235, 235, 235),
 
-	--// Text:
-	Tab = Color3.fromRGB(200, 200, 200),
-	Title = Color3.fromRGB(240,240,240),
-	Description = Color3.fromRGB(200,200,200),
+		--// Text:
+		Tab = Color3.fromRGB(50, 50, 50),
+		Title = Color3.fromRGB(0, 0, 0),
+		Description = Color3.fromRGB(100, 100, 100),
 
-	--// Outlines:
-	Shadow = Color3.fromRGB(0, 0, 0),
-	Outline = Color3.fromRGB(40, 40, 40),
+		--// Outlines:
+		Shadow = Color3.fromRGB(255, 255, 255),
+		Outline = Color3.fromRGB(210, 210, 210),
 
-	--// Image:
-	Icon = Color3.fromRGB(220, 220, 220),
+		--// Image:
+		Icon = Color3.fromRGB(100, 100, 100),
+	},
+	
+	Dark = {
+		--// Frames:
+		Primary = Color3.fromRGB(30, 30, 30),
+		Secondary = Color3.fromRGB(35, 35, 35),
+		Component = Color3.fromRGB(40, 40, 40),
+		Interactables = Color3.fromRGB(45, 45, 45),
+
+		--// Text:
+		Tab = Color3.fromRGB(200, 200, 200),
+		Title = Color3.fromRGB(240,240,240),
+		Description = Color3.fromRGB(200,200,200),
+
+		--// Outlines:
+		Shadow = Color3.fromRGB(0, 0, 0),
+		Outline = Color3.fromRGB(40, 40, 40),
+
+		--// Image:
+		Icon = Color3.fromRGB(220, 220, 220),
+	},
+
 }
 
 --// Sections
@@ -91,13 +114,32 @@ Window:AddInput({
 
 --// Tab [SETTINGS]
 
-local Main = Window:AddTab({
+local Edit = Window:AddTab({
 	Title = "Edit",
 	Section = "Settings",
 	Icon = "rbxassetid://11432847075",
 })
 
+Window:AddDropdown({
+	Title = "Test",
+	Description = "PLease select",
+	Tab = Edit,
+	Options = {
+		["This is the name!"] = "And this is the value that gets sent!",
+		
+		
+		["aahhahaha"] = "Value",
+		["cocos pov dropdown"] = "whos cocos pov",
+		["HMMM"] = "i clciked hmmm",
+		["Lol."] = "lol lol",
+		["hello"] = "hi",
+		["wha tthe"] = "what",
+		["brrbrbbr"] = "brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr",
 
+	},
+	Callback = function(Option) 
+		warn(Option)
+	end,
+}) 
 
-Window:SetTheme(CustomTheme)
-
+Window:SetTheme(Themes.Light)
